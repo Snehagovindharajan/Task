0b5d62e42d142098468b886ef60db0f0de8c0f93
Snehagovindharajan <snehag2205@gmail.com>
2019-11-14 09:17:50 +0530

    added some files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mymath_task_x_power_y",
+    version="1.0",
+    packages=find_packages(),
+    py_modules = ['Task.function_x_power_y:main']
+)
