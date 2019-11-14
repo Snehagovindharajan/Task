141e9c72a43443b04741a8d66599a82c56aaf018
Snehagovindharajan <snehag2205@gmail.com>
2019-11-14 11:19:27 +0530

    changed some files

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="mymath_task_x_power_y",
     version="1.0",
     packages=find_packages(),
-    py_modules = ['Task.function_x_power_y:main']
+    py_modules=['function_x_power_y:main']
 )
