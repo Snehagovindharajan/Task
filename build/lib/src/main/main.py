465c162bf08c2727a6bb6e5757a6a201699b98ae
Snehagovindharajan <snehag2205@gmail.com>
2019-11-19 11:24:29 +0530

    changed some files

--- build/lib/src/main/main.py
+++ build/lib/src/main/main.py
@@ -1,10 +1,25 @@
 import sys
-from src.driver.ScientificCalc import ScientificFunction
+from src.driver.ScientificCalc import ScientificCalc
+import argparse
+import logging
 
+
+logging.basicConfig(filename='ScientificCalculatorLog.log', level=logging.ERROR, format='%(name)s - %(levelname)s - '
+                                                                                        '%(message)s - %(asctime)s - '
+                                                                                        '%(lineno)d - %(module)s - %('
+                                                                                        'funcName)s - %(pathname)s')
 
 def main():
-    obj_power = ScientificFunction()
-    input_base = sys.argv[1]
-    input_power = sys.argv[2]
-    final_answer = obj_power.x_power_y(input_base, input_power)
-    print(final_answer)
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--function', type=str, required=True, nargs='+')
+        args = parser.parse_args()
+        method_name = args.function
+        obj_power = ScientificCalc()
+        if method_name[0] == 'x_power_y':
+            input_base = method_name[1]
+            input_power = method_name[2]
+            final_answer = obj_power.x_power_y(input_base, input_power)
+            print(final_answer)
+    except IndexError as index:
+        logging.error(index)
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="mymath_task_x_power_y",
-    version="1.0",
+    version="1.2",
     packages=find_packages(),
-    data_files=[('.', ["__main__.py", "ReadMe.md", 'ScientificFunctionLog.log', 'setup.py'])],
+    data_files=[('.', ["__main__.py", "ReadMe.md", 'setup.py', 'requirements.txt'])],
     entry_points={"setuptools.installation": ["eggsecutable=src.main.main:main"]}
 )
