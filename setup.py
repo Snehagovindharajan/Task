46abe5bd94c754f63c45fecf3dc500d5f1063b28
Snehagovindharajan <snehag2205@gmail.com>
2019-11-20 22:18:53 +0530

    created x_power_y project

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="function_x_power_y",
-    version="1.2",
+    version="1.3",
     packages=find_packages(),
     data_files=[('.', ["__main__.py", "ReadMe.md", 'setup.py', 'requirements.txt'])],
     entry_points={"setuptools.installation": ["eggsecutable=src.main.main:main"]}
