a14d426b3387bff94d6583ef492c94e391665def
Snehagovindharajan <snehag2205@gmail.com>
2019-11-21 11:12:31 +0530

    changed some files

--- build/lib/test/main/main.py
+++
@@ -1,7 +0,0 @@
-"""main.py to call the unittest case Class"""
-from test.driver.scientific_calc_test import ScientificCalcTest
-import unittest
-
-if __name__ == '__main__':
-    TESTCLASS_OBJ = ScientificCalcTest()
-    unittest.main()
